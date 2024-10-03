d638716c6b783b8b2b9c4cf23a8d0db908447f53
Sergey Khromenkov <khromenkov00@bk.ru>
2024-10-03 09:48:47 +0200

    Initial commit

---
+++ Homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+try
+{
+Console.WriteLine("Введите значение H");
+int H = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение R");
+int R = int.Parse(Console.ReadLine());
+double V= Math.PI * Math.Pow(R,2)* H;
+double V2 = (Math.PI * Math.Pow(R,2) * H)/3;
+Console.WriteLine($"Объем конуса V2={V2:F2}");
+Console.WriteLine($"Объем цилиндра V={V:F2}");
+}
+catch
+{
+    
+}
